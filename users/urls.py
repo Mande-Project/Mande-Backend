5cec392ab9d7b1257295119536c2cc7ddc51b62d
MauMG03 <mauricio.munoz.gutierrez@correounivalle.edu.co>
2023-10-08 02:49:49 -0500

    fix: eliminated some unuseful documentation

--- users/urls.py
+++ users/urls.py
@@ -16,14 +16,3 @@ urlpatterns = [
 ]
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
-
-""" from .views import customer_register, customer_login, worker_register, worker_login,  user_logout
-
-urlpatterns = [
-    path('customer_register/', customer_register, name='customer_register'),
-    path('customer_login/', customer_login, name='customer_login'),
-    path('worker_register/', worker_register, name='worker_register'),
-    path('worker_login/', worker_login, name='worker_login'),
-    
-    path('logout/', user_logout, name='user_logout'),
-] """
--- users/views.py
+++ users/views.py
@@ -30,7 +30,7 @@ class CustomerViewSet(APIView):
                     }
                     return Response({"status":"success","data":user})
                 else:
-                    return HttpResponse('No matching customer and password were found', status=401)
+                    return HttpResponse(f'No matching customer with id {id} found', status=401)
                 
             except:      
                 customers = Customer.objects.all()
@@ -68,7 +68,7 @@ class WorkerViewSet(APIView):
                     }
                     return Response({"status":"success","data":user})
                 else:
-                    return HttpResponse('No matching customer and password were found', status=401)
+                    return HttpResponse(f'No matching worker with id {id} found', status=401)
                 
             except:      
                 workers = Worker.objects.all()
@@ -190,145 +190,3 @@ class CustomUserViewSet(UserViewSet):
             return CustomUserCreateSerializer
         else:
             return CustomUserSerializer          
-
-""" 
-# Component: customer_register
-# Description: View - Endpoint that creates new users for new Customer instances
-# Author: paul.rojas@correounivalle.edu.co, paulrodrigorojasecl@gmail.com
-
-@api_view(['POST'])
-def customer_register(request):
-    if request.method == 'POST':
-        try:
-
-            data = {
-                'email':request.data.get('email'),
-                'password':request.data.get('password'),
-                'phone':request.data.get('phone'),
-                'username':request.data.get('username'),
-                'first_name':request.data.get('first_name'),
-                'last_name':request.data.get('last_name')
-            }
-
-            user = create_user(data)
-
-            if user:
-
-                client = Customer(
-                    user = user,
-                )
-
-                client.save()
-
-                return HttpResponse("Customer successfully registered", status=200)
-            else:
-                return HttpResponse("That user has already been registered", status=400)    
-            
-        except Exception as e:
-            print(e)
-            return HttpResponse("An error has ocurred", status=400)
-        
-    else:
-        return HttpResponse("Unsupported method", status=405)
-               
-
-
-# Component: worker_register
-# Description: View - Endpoint that creates new users for new Worker instances
-# Author: paul.rojas@correounivalle.edu.co, paulrodrigorojasecl@gmail.com
-
-@api_view(['POST'])
-def worker_register(request):
-    if request.method == 'POST':
-        try:
-
-            data = {
-                'email':request.data.get('email'),
-                'password':request.data.get('password'),
-                'phone':request.data.get('phone'),
-                'username':request.data.get('username'),
-                'first_name':request.data.get('first_name'),
-                'last_name':request.data.get('last_name')
-            }
-
-            user = create_user(data)
-
-            if user:
-
-                client = Worker(
-                    user = user,
-                )
-
-                client.save()
-
-                return HttpResponse("Worker successfully registered", status=200)
-            else:
-                return HttpResponse("That user has already been registered", status=400)    
-            
-        except Exception as e:
-            print(e)
-            return HttpResponse("An error has ocurred", status=400)
-        
-    else:
-        return HttpResponse("Unsupported method", status=405)     
-        
-
-# Component: customer_login
-# Description: View - Endpoint that allows to create a session in the application for a Customer user
-# Author: paul.rojas@correounivalle.edu.co, paulrodrigorojasecl@gmail.com
-
-@api_view(['POST'])
-def customer_login(request):
-    if request.method == 'POST':
-        email = request.data.get('email')
-        password = request.data.get('password')
-
-        user = authenticate(email=email, password=password);
-
-        if Customer.objects.filter(user__email=email).exists():
-            user = authenticate(email=email, password=password)
-
-            if user:
-                token, _ = Token.objects.get_or_create(user=user)
-                return JsonResponse({'token':token.key}, status=200)
-
-        return HttpResponse('No matching customer and password were found', status=401)
-    
-
-# Component: worker_login
-# Description: View - Endpoint that allows to create a session in the application for a Worker user
-# Author: paul.rojas@correounivalle.edu.co, paulrodrigorojasecl@gmail.com
-
-@api_view(['POST'])
-def worker_login(request):
-    if request.method == 'POST':
-        email = request.data.get('email')
-        password = request.data.get('password')
-
-        user = authenticate(email=email, password=password);
-
-        if Worker.objects.filter(user__email=email).exists():
-            user = authenticate(email=email, password=password)
-
-            if user:
-                token, _ = Token.objects.get_or_create(user=user)
-                return JsonResponse({'token':token.key}, status=200)
-
-        return HttpResponse('No matching customer and password were found', status=401)
-    
-    
-
-# Component: user_logout
-# Description: View - Endpoint that logouts from the current session
-# Author: paul.rojas@correounivalle.edu.co, paulrodrigorojasecl@gmail.com
-
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def user_logout(request):
-    if request.method == 'POST':
-        try:
-            request.user.auth_token.delete()
-            return HttpResponse('User was successfully logout')
-        except Exception as e:
-            return HttpResponse('Internal error ocurred', status=500) """
